2dc4c99b6b92b73fe2a782281becc09205ee07f4
victuk <ukokjnr@gmail.com>
2019-11-21 18:23:56 +0100

    I have not tested it fully with postman but the server is running without errors.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+//import jwt from './_helpers/jwt';
+//import errorHandler from './_helpers/error-handler';
 
-import router from './routers';
+import router from './routers/index';
 
 const app = express();
 
@@ -11,6 +14,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+//app.use(jwt());
+
+// api routes
+app.use('/users', require('./models/User'));
+
+// global error handler
+//app.use(errorHandler);
 
 app.use('/api/v1', router);
 
