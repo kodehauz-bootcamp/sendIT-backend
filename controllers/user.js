f36308a847a037e99555b8407d28006e4fdfdd8a
Tueloper <ozurumbatochukwu@yahoo.com>
2019-12-04 15:02:53 +0100

    auth fixed

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,72 @@
 const User = require('./../models/user');
 const generateAuthToken = require('./../utils/generateToken');
-const hash = require('./../utils/hash')
+const {hash, compareHash} = require('./../utils/hash');
 
 module.exports = {
 
   async CreateUser(request, response) {
 
+    const { password, passwordConfirm, username, email } = request.body;
+
     try {
-      const user = new User(request.body);
+      if (password != passwordConfirm) {
+        return response.status(402).send({message: 'Password is not correctly alligned'})
+      }
+      const user = new User({
+        username: username,
+        email: email,
+        password: password
+      });
       const token = await generateAuthToken(user);
       await hash(user);
 
       await user.save();
-      response.status(200).send({ message: "Success", user, token})
+      response.status(201).send({ message: "Success", user, token})
+    } catch (e) {
+      response.status(500).send(e.message);
+    }
+  },
+
+  async loginUser(request, response) {
+    try {
+
+      const { email, password } = request.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user ) {
+        throw new Error("You are not Authorised");
+      }
+
+      const passwordMatch = await compareHash(password, user.password);
+      // return console.log(passwordMatch)
+
+      if (!passwordMatch) {
+        throw new Error("Invalid Password");
+      }
+      const token = await generateAuthToken(user);
+
+      response.status(201).send({ message: "Success", user, token})
     } catch (e) {
       response.status(500).send(e.message)
+    }
+  },
+
+  async getUser ( request, response) {
+    response.send(request.user)
+  },
+
+  async userLogout (request, response) {
+    // return console.log(userProfile)
+    try {
+      const userProfile = request.user
+      userProfile.tokens = [];
+      await userProfile.save();
+      return response.status(200).send(userProfile);
+    } catch (e) {
+        response.status(400).send(e.message)
     }
   }
 
 };
+
--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,15 @@
-import { Router } from 'express';
-
-import { CreateUser } from '../controllers/user';
+const { Router } = require('express');
+const Auth = require('./../middleware/Auth');
+const { CreateUser, loginUser, userLogout, getUser } = require('../controllers/user');
 
 const router = Router();
 
 router.post('/signup', CreateUser );
+
+router.post('/login', loginUser );
+
+router.get('/me', Auth, getUser );
+
+router.delete('/logout', Auth, userLogout );
 
 export default router;
