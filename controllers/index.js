7dd5a393b78b5ef48d17c21d9b2c37111dc00af0
Tueloper <ozurumbatochukwu@yahoo.com>
2019-12-04 15:43:57 +0100

    heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,5 @@
-export const welcomeMessage = (request, response) => {
+const welcomeMessage = (request, response) => {
   response.send('welcome to sendIT-backend');
 }
+
+module.exports = welcomeMessage;
--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const { welcomeMessage } = require('../controllers');
+const welcomeMessage = require('../controllers');
 
 const router = Router();
 
