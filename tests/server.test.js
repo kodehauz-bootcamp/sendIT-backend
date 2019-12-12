46c294e8de8f37c16f5aafd762efc06b51623d3b
Tueloper <ozurumbatochukwu@yahoo.com>
2019-12-12 09:34:44 +0100

    Test trail

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,17 +1,17 @@
-import { describe, it } from 'mocha';
-import chai, { expect } from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../src/app';
+const { describe, it } = require('mocha');
+const { chai, expect } = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./../index');
 
 chai.use(chaiHttp);
 
 describe('App', () => {
-	it('Should display Chess9ja node server', (done) => {
+	it('Should display Welcome node server Message', (done) => {
 		chai.request(app).get('/api/v1').end((err, res) => {
 			expect(res.status).eql(200);
 			expect(res.body).to.be.an('object');
 			expect(res.body.status).to.eql('success');
-			expect(res.body.data).to.eql('Chess9ja node server');
+			expect(res.body.data).to.eql('Welcome to sendIT-backend');
 			done();
 		});
 	});
