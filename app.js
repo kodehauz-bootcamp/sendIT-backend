e62cbcb8df865ad49e87ffa5c6e24456fea1483f
Tueloper <ozurumbatochukwu@yahoo.com>
2020-01-17 01:07:46 +0100

    port config

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const app = require('./index');
 const chalk = require('chalk');
 
-const PORT = process.env.PORT || 5001;
+// const PORT = process.env.PORT || 5001;
+app.set('port', process.env.PORT || 5000);
 
-app.listen(PORT, () => {
-	console.log(chalk.cyan.italic(`Server Started At ${PORT}`));
-});
+// app.listen(PORT, () => {
+// 	console.log(chalk.cyan.italic(`Server Started At ${PORT}`));
+// });
